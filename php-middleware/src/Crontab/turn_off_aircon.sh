be3fba2df466c506367fc568a793da9aff178097
yinghao <yinghao@ciandt.com>
2024-06-05 16:42:17 +0800

    关闭空调定时任务

--- php-middleware/src/Crontab/turn_off_aircon.sh
+++ php-middleware/src/Crontab/turn_off_aircon.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 set -e
-a=$(docker ps -qf name=citcdoffice-2102_mqtt_1)
-docker exec -it citcdoffice-2102_mqtt_1 mosquitto_pub -t "zigbee2mqtt/0x70ac08fffe65a18c/set" -m '{"ir_code_to_send":"DQMSAxIjAj4GIwIjAlgCQAdAAwQjAlgCIyABgAtAAQFYAuAHC8AP4AEHgCMBPgaAA0ABwAtAB4ABAlgCI6ABgBOAD0ALwAOAAYAX4AUBgBOAJwBYoAeADwEDEkABASMCQA9AAUAHQAOAAYArwAHgARcBIwJAC0ADQAHgAwdAC8ADQAGAC+AFR8ABAz4GIwLAAUALwAPgFQGAS8ArBz4GIwI+BiMC"}'
-echo "$a"
+docker exec -it mosquitto -t "zigbee2mqtt/0x70ac08fffe65a18c/set" -m '{"ir_code_to_send":"DQMSAxIjAj4GIwIjAlgCQAdAAwQjAlgCIyABgAtAAQFYAuAHC8AP4AEHgCMBPgaAA0ABwAtAB4ABAlgCI6ABgBOAD0ALwAOAAYAX4AUBgBOAJwBYoAeADwEDEkABASMCQA9AAUAHQAOAAYArwAHgARcBIwJAC0ADQAHgAwdAC8ADQAGAC+AFR8ABAz4GIwLAAUALwAPgFQGAS8ArBz4GIwI+BiMC"}'
