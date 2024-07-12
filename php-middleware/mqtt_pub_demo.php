e90fd341dcbc87bd9ecec4c5db38a958a2a29c0b
yinghao <yinghao@ciandt.com>
2024-07-12 08:41:15 +0800

    制冷23度

--- php-middleware/mqtt_pub_demo.php
+++ php-middleware/mqtt_pub_demo.php
@@ -1,4 +1,5 @@
 <?php
+
 define('BASE_PATH', __DIR__);
 
 use Swoole\Coroutine;
@@ -10,34 +11,16 @@ $client = [];
 
 try {
   $client = new Mosquitto\Client();
-  $client->setCredentials('admin', '123456');
-  $client->setWill('dead', 'I am offline ', 1, false);
-  $client->connect('zigbee2mqtt_mqtt_1', 1883, 60);
-  // $client->connect('192.168.20.68', 1883, 60);
+  $client->setCredentials('admin', 'o8Uslg');
+  $client->connect('mosquitto', 1883, 60);
   $g = [
-    'friendly_name' => '0x70ac08fffe65a18c',
-    'payload' => [
-      'version' => 7,
-      'timestamp' => time(),
-      'state' => [
-        'learned_ir_code' => 'CTkSORL+AcMG/gFAAUAHQAMH/gG6AYcCugFACwOHAv4BQAGACwG6AUADQAtAA4ALC3EBwwa6AYcC/gHDBuADAwC6IA+AGwj9AroBwwb+AYfgAAdACwBxIA8DcQHDBoAbA/0C9QBACwKHAv6gA0ALAHEgC4AHCcMGcQGHAv4BwwbgAwMNugHfFO8H9QC6AboBkAPgAcsMcQHvB2UAwwb+AYcCcSADBboBwwbRAEAHQAMFwwa6AYcCwBcRugHDBnEBOws3AcMGugGHAroBQAeAAwBxIAcL/gHvB/UAwwZxAYcCgCMDOwu6AUATBYcCugHDBoADA4cCcQFACwL9AjcgAwPRAMMGQA8LNwHDBroBhwK6Ae8HQBME/gHDBrogAws3Ad8UMwX1AP4BqhpAD4ADA4cC/gFACw/9AvUA7wf1AIcCNwE5EroBQBMPhwK6ATsLNwHDBv4B/QI3AUAPQAMBORLAZwlxATsLugH9AnEBQAsLwwb1AJ4sugE7C3EB',
-      ],
-      'metadata' => [
-        'switch' => [
-          'timestamp' => time(),
-        ],
-      ]
-    ],
+    'learned_ir_code' => 'EYkSiRLuAYkGRgLuAUYCiQbuAUADAUYCgAOAC0AB4AMLAJlgF4AbQAcDRgKJBuADB0ALwAOAGwFGAoADA7ACmQHgCwvgAycDiQbuAYAjQAEDmQGwAsAPAe4BwBtABwWJBkYCiQZAByADCxImCdoAngDaAJkB7iABAYkSIAMgFwNGAu4BQAdAA0ALQANACwKwApkgAwHuAUALQBMDRgKZAUALA4kGmQHAFwGJBkADAJmgBwFGAoADwA8D7gFGAuAFAwOwApkBQBMCsAJHIAME7gGJBpkgA0ARCu4BiQbuAbACmQFGYAMH7gHFA9oAsALAJwXuAbAC7gFAB',
   ];
 
   $m = json_encode($g);
 
-  // $t = 'java_middleware/router_things/shadow/update/delta';
   $client->publish($t, $m, 1);
-
-  $t = 'zigbee2mqtt/0x00158de67719693b/set';
-  $client->publish($t, $m, 1);
-
-} catch (\Mosquitto\Exception $exception) {
+}
+catch (\Mosquitto\Exception $exception) {
   var_dump('err', $exception->getMessage());
 }
--- php-middleware/src/Crontab/turn_off_aircon.sh
+++ php-middleware/src/Crontab/turn_off_aircon.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-docker exec -it mosquitto mosquitto_pub -t "zigbee2mqtt/0x70ac08fffe65a18c/set" -m '{"ir_code_to_send":"DQMSAxIjAj4GIwIjAlgCQAdAAwQjAlgCIyABgAtAAQFYAuAHC8AP4AEHgCMBPgaAA0ABwAtAB4ABAlgCI6ABgBOAD0ALwAOAAYAX4AUBgBOAJwBYoAeADwEDEkABASMCQA9AAUAHQAOAAYArwAHgARcBIwJAC0ADQAHgAwdAC8ADQAGAC+AFR8ABAz4GIwLAAUALwAPgFQGAS8ArBz4GIwI+BiMC"}'
+docker exec -i mosquitto mosquitto_pub -t "zigbee2mqtt/0x70ac08fffe65a18c/set" -m '{"ir_code_to_send":"DQMSAxIjAj4GIwIjAlgCQAdAAwQjAlgCIyABgAtAAQFYAuAHC8AP4AEHgCMBPgaAA0ABwAtAB4ABAlgCI6ABgBOAD0ALwAOAAYAX4AUBgBOAJwBYoAeADwEDEkABASMCQA9AAUAHQAOAAYArwAHgARcBIwJAC0ADQAHgAwdAC8ADQAGAC+AFR8ABAz4GIwLAAUALwAPgFQGAS8ArBz4GIwI+BiMC"}'
 
 datetime=$(date +%Y-%m-%d-%H:%M:%S)
 curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=92e83753-ba9f-41e8-b146-75c37b17f8ef' \
